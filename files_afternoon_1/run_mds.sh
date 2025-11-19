636703c711b652b59c260ac47ea5a5f167030f06
is2364 <philipp.dullinger@kit.edu>
2025-11-19 11:48:28 +0100

    added posres and counting

--- files_afternoon_1/run_mds.sh
+++ files_afternoon_1/run_mds.sh
@@ -1,10 +1,27 @@
-steps="steep nvt npt_ber npt"
+steps=" nvt npt_ber_posres npt_ber npt"
 startgro="solvated_full_system.gro"
+
+#first we need a  dummy tpr
+#gmx grompp -f nvt.mdp -c $startgro -p full_system.top -o dummy.tpr -maxwarn 1
+#cp full_system.top full_system_ions.top
+#echo "SOL" | gmx genion -s dummy.tpr  -o full_system_ions.gro -p full_system_ions.top  -neutral  yes -conc 0.050
+
+startgro="full_system_ions.gro"
+#read -p "works ?"
+
+
 for part in $steps
 do
-gmx grompp -f $part.mdp -c $startgro -p  full_system.top -o $part.tpr
+gmx grompp -f $part.mdp -c $startgro -p  full_system_ions.top -o $part.tpr -r $startgro
 read -p works?
 startgro=$part.gro
-gmx mdrun -deffnm $part -v 
+gmx mdrun -deffnm $part -v -cpi
 done
 
+steps="npt_ber npt" # no more position restraints here!
+for part in $steps
+do
+gmx grompp -f $part.mdp -c $startgro -p  full_system_ions.top -o $part.tpr 
+startgro=$part.gro
+gmx mdrun -deffnm $part -v -cpi
+done
